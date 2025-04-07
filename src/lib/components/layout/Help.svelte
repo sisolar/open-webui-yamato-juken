f91a40c247bac3306b0f2a420aea88c02bcb05dc
John Doe <johndoe@example.com>
2025-04-07 14:00:30 +0900

    右下のヘルプボタンを削除

--- src/lib/components/layout/Help.svelte
+++ src/lib/components/layout/Help.svelte
@@ -10,31 +10,4 @@
 	let showShortcuts = false;
 </script>
 
-<div class=" hidden lg:flex fixed bottom-0 right-0 px-2 py-2 z-20">
-	<button
-		id="show-shortcuts-button"
-		class="hidden"
-		on:click={() => {
-			showShortcuts = !showShortcuts;
-		}}
-	/>
-
-	<HelpMenu
-		showDocsHandler={() => {
-			showShortcuts = !showShortcuts;
-		}}
-		showShortcutsHandler={() => {
-			showShortcuts = !showShortcuts;
-		}}
-	>
-		<Tooltip content={$i18n.t('Help')} placement="left">
-			<button
-				class="text-gray-600 dark:text-gray-300 bg-gray-300/20 size-5 flex items-center justify-center text-[0.7rem] rounded-full"
-			>
-				?
-			</button>
-		</Tooltip>
-	</HelpMenu>
-</div>
-
 <ShortcutsModal bind:show={showShortcuts} />
